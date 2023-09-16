b7d40d1d5f32c5f2aba2576893d24320c300626c
Arnold van der Merwe <arnold1.vandermerwe@gmail.com>
2023-09-16 14:05:01 +0200

    src/pages/AdminRatesPage: Added dialogs for editing and deleting
    The dialogs to edit and delete a row were added.
    
    Signed-off-by: Arnold van der Merwe <arnold1.vandermerwe@gmail.com>

--- src/pages/AdminRatesPage.vue
+++ src/pages/AdminRatesPage.vue
@@ -30,7 +30,7 @@
             />
             <q-input
               filled
-              v-model="inputLoadSheddingRate"
+              v-model="inputLoadsheddingRate"
               label="Electrical rate during load shedding (c/kWh)"
             />
             <div class="row flex-center">
@@ -70,6 +70,60 @@
         </template>
       </q-table>
     </q-list>
+    <q-dialog v-model="editPrompt">
+      <q-card style="min-width: 400px">
+        <q-card-section>
+          <div class="text-h6">Edit time slot</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-form class="q-gutter-md">
+            <q-input
+              filled
+              v-model="editStartTime"
+              label="Start time [hh:mm]"
+              class="col q-mr-md"
+            />
+            <q-input
+              filled
+              v-model="editEndTime"
+              label="End time [hh:mm]"
+              class="col q-ml-md"
+            />
+            <q-input
+              filled
+              v-model="editNormalRate"
+              label="Normal electrical rate (c/kWh)"
+            />
+            <q-input
+              filled
+              v-model="editLoadsheddingRate"
+              label="Electrical rate during load shedding (c/kWh)"
+            />
+          </q-form>
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Confirm" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-dialog v-model="deletePrompt">
+      <q-card style="min-width: 350px">
+        <q-card-section class="row items-center">
+          <q-avatar icon="delete" color="accent" text-color="white" />
+          <span class="q-ml-sm"
+            >Are you sure you want to delete this time slot?
+          </span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Delete" color="accent" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
@@ -113,7 +167,27 @@ export default defineComponent({
           loadsheddingRate: 200,
         },
       ],
+      editPrompt: false,
+      editStartTime: null,
+      editEndTime: null,
+      editNormalRate: null,
+      editLoadsheddingRate: null,
+      deletePrompt: false,
     };
+  },
+
+  methods: {
+    onEdit(row) {
+      this.editStartTime = row.start;
+      this.editEndTime = row.end;
+      (this.editNormalRate = row.normalRate),
+        (this.editLoadsheddingRate = row.loadsheddingRate);
+      this.editPrompt = true;
+    },
+
+    onDelete(row) {
+      this.deletePrompt = true;
+    },
   },
 });
 </script>
